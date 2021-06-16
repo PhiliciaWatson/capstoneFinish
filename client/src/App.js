6ec5be2735e278225aca0f29ce259e92fa377ff9
PhiliciaWatson <philiciawatson7@gmail.com>
2021-06-16 13:37:37 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 // IMPORTS BELOW ARE FOR WEATHER APP 
 //import 'weather-icons/css/weather-icons.css';
 // IMPORTS ABOVE ARE FOR WEATHER APP ^
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './footer'; //Include Footer
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './home';
@@ -17,26 +17,27 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Attractions from './Attractions';
 import addAttract from './addAttract';
 import Burger from './components/Burger';
-// import SingleAttraction  from './singleatt';
+import SingleAttraction  from './singleatt';
 
 
 
 
-class App extends Component {
+
+class App extends React.Component {
   
   
   render() {
     return (
      <Router>
       <div className="maincontainer">
-        <Burger/>
+        { <Burger/> }
 
-        <Route exact path='/' component={Home} />
+        <Route exact path='/home' component={Home} />
         <Route exact path='/about' component={About} />
         <Route exact path='/Contact' component ={Contact}/>
         <Route exact path='/attractions' component ={Attractions}/>        
         <Route exact path ='/add' component= {addAttract}/>
-        {/* <Route path = '/attractions/:id' component={SingleAttraction}/> */}
+        <Route path = '/attractions/:id' component={SingleAttraction}/>
        
 
 
--- client/src/app_component/weather.component.js
+++ client/src/app_component/weather.component.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import './weather.css';
 
 
 const Weather = (props) =>{
     return(
-       <div className="container">
+       <div className="container" id="container">
         <div className="cards pt-4">
-           <h1>{props.city}</h1>
+        <h1 style={{fontWeight: 'bold', fontFamily: '"Cormorant SC", serif'}}>
+                {props.city} , {props.country}</h1>
            <h5 className="py-4">
-               <i className={`wi ${props.weatherIcon}display-1`}/>
+               <i className={`wi ${props.weatherIcon} display-1`}/>
            </h5>
            {props.temp_celsius ?(
-            <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+            <h1 className="py-2">{props.temp_celsius}<span className="blink_me">&deg;</span> C </h1>
+           ):null}
+            {/* <br/> */}
+           {props.Fahrenheit ?(
+            <h3 className="py-2">{props.Fahrenheit}<span className="blink_me">&deg;</span> F </h3>
            ):null}
             {/* {show max and min temp} */}
+            <h5 style={{fontWeight:'900'}}>Temperature ranges</h5>
             {minmaxTemp(props.temp_min, props.temp_max)}
-            <h4 className="py-4">{props.description}</h4>
+            <h4 className="py-4" style={{fontWeight: 'bold', fontFamily: '"Cormorant SC", serif'}}>{props.description}</h4>
         </div>
        </div>
     );
@@ -24,8 +31,8 @@ const minmaxTemp = (min, max) =>{
     if(min && max){
         return(
             <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
+            <span className="px-4">{min}<span className="blink_me">&deg;</span></span>
+            <span className="px-4">{max}<span className="blink_me">&deg;</span></span>
         </h3>
         );
     }
